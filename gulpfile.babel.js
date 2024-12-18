defe4c41e3e66b3daaf054d64d5957c6a5b23484
펜타브리드_강선율 <yein1212k@gmail.com>
2024-12-18 18:47:35 +0900

    test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,8 @@ const gulp = require("gulp");
 const scss = require("gulp-sass")(require("sass"));
 const babel = require("gulp-babel");
 const sourcemaps = require("gulp-sourcemaps");
-const fileinclude = require("gulp-file-include"); //변수전달 기능이 안됨. 사용안함
 const htmlbeautify = require("gulp-html-beautify");
 const ejs = require("gulp-ejs");
-const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 const autoprefixer = require("gulp-autoprefixer");
 const rename = require("gulp-rename");
@@ -13,198 +11,115 @@ const browserSync = require("browser-sync").create();
 const ghPages = require("gulp-gh-pages");
 const del = require("del");
 
+// Folder paths
 const SRC_FOLDER = "./src";
 const DIST_FOLDER = "./dist";
 
 const SRC_PATH = {
-    ASSETS: {
-        FONTS: "./src/assets/fonts",
-        IMAGES: "./src/assets/images",
-        SCSS: "./src/assets/scss",
-        JS: "./src/assets/js",
-        MOVIES: "./src/assets/movies",
-    },
-    EJS: "./src/ejs",
+  ASSETS: {
+    FONTS: "./src/assets/fonts",
+    IMAGES: "./src/assets/images",
+    SCSS: "./src/assets/scss",
+    JS: "./src/assets/js",
+    MOVIES: "./src/assets/movies",
   },
-  DEST_PATH = {
-    ASSETS: {
-        FONTS: "./dist/assets/fonts",
-        IMAGES: "./dist/assets/images",
-        CSS: "./dist/assets/css",
-        JS: "./dist/assets/js",
-        MOVIES: "./dist/assets/movies",
-    },
+  EJS: "./src/ejs",
+};
+
+const DEST_PATH = {
+  ASSETS: {
+    FONTS: "./dist/assets/fonts",
+    IMAGES: "./dist/assets/images",
+    CSS: "./dist/assets/css",
+    JS: "./dist/assets/js",
+    MOVIES: "./dist/assets/movies",
   },
-  // 옵션
-  OPTIONS = {
-    outputStyle: "expanded",
-    indentType: "space",
-    indentWidth: 4,
-    precision: 8,
-  };
+};
 
-gulp.task("clean", function () {
-  return del(["dist"]);
-});
+// -------------------- Clean Tasks --------------------
+gulp.task("clean", () => del([DIST_FOLDER]));
+gulp.task("cleanDeploy", () => del(".publish"));
 
-gulp.task("html", () => {
+// -------------------- EJS Task --------------------
+gulp.task("ejs", () => {
+  console.log("Compiling EJS files...");
   return gulp
-    .src([SRC_FOLDER + "**/*.html"], {
-      base: SRC_FOLDER,
-      since: gulp.lastRun("html"),
-    })
-    .pipe(gulp.dest(DIST_FOLDER))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("ejs", function () {
-  return gulp
-    .src([SRC_PATH.EJS + "/**/!(_)*.ejs", SRC_PATH.EJS + "/*.ejs"])
-    .pipe(ejs({
-      DOCUMENT_ROOT: '/' // 로컬 환경에서는 기본적으로 / 사용
-    }))
-    .pipe(rename({ extname: ".html" }))
-    .pipe(
-      fileinclude({
-        prefix: "@@", // 사용할 때 @@ 붙이면 됨
-        basepath: "@file",
-      })
-    )
-    .pipe(htmlbeautify({ indentSize: 2 }))
-    .pipe(gulp.dest(DIST_FOLDER))
-    .pipe(browserSync.stream());
-});
-
-gulp.task("ejs-prod", function () {
-  return gulp
-    .src([SRC_PATH.EJS + "/**/!(_)*.ejs", SRC_PATH.EJS + "/*.ejs"])
-    .pipe(ejs({
-      DOCUMENT_ROOT: '/yonhapnews/'  // 배포 환경에서는 /yonhapnews/ 사용
+    .src([`${SRC_PATH.EJS}/**/!(_)*.ejs`], { base: SRC_PATH.EJS }) // Exclude partials
+    .pipe(ejs({ DOCUMENT_ROOT: "./" }).on("error", (err) => {
+      console.error("EJS Compilation Error:", err.message);
+      this.emit("end");
     }))
     .pipe(rename({ extname: ".html" }))
     .pipe(htmlbeautify({ indentSize: 2 }))
-    .pipe(gulp.dest(DIST_FOLDER))
+    .pipe(gulp.dest(DIST_FOLDER)) // Output to dist folder
     .pipe(browserSync.stream());
 });
 
-gulp.task("scss:compile", function () {
+
+// -------------------- SCSS Task --------------------
+gulp.task("scss:compile", () => {
   return gulp
-    .src(SRC_PATH.ASSETS.SCSS + "/*.scss")
+    .src(`${SRC_PATH.ASSETS.SCSS}/*.scss`)
     .pipe(sourcemaps.init())
-    .pipe(scss({ silenceDeprecations: ['legacy-js-api'] }).on('error', scss.logError))
+    .pipe(scss().on("error", scss.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(DEST_PATH.ASSETS.CSS))
     .pipe(browserSync.stream());
 });
 
+// -------------------- JS Task --------------------
 gulp.task("js", () => {
   return gulp
-    .src([
-      SRC_PATH.ASSETS.JS + "/**/*.js",
-    ])
+    .src(`${SRC_PATH.ASSETS.JS}/**/*.js`)
     .pipe(babel())
     .pipe(uglify())
     .pipe(gulp.dest(DEST_PATH.ASSETS.JS))
     .pipe(browserSync.stream());
 });
 
-gulp.task("images", () => {
-  return gulp
-    .src(SRC_PATH.ASSETS.IMAGES + "/**/*.+(png|jpg|jpeg|gif|ico)")
-    .pipe(gulp.dest(DEST_PATH.ASSETS.IMAGES))
-    .pipe(browserSync.stream());
+// -------------------- Assets Tasks --------------------
+gulp.task("images", () => gulp.src(`${SRC_PATH.ASSETS.IMAGES}/**/*`).pipe(gulp.dest(DEST_PATH.ASSETS.IMAGES)));
+gulp.task("fonts", () => gulp.src(`${SRC_PATH.ASSETS.FONTS}/**/*`).pipe(gulp.dest(DEST_PATH.ASSETS.FONTS)));
+gulp.task("movies", () => gulp.src(`${SRC_PATH.ASSETS.MOVIES}/*`).pipe(gulp.dest(DEST_PATH.ASSETS.MOVIES)));
+
+// -------------------- BrowserSync --------------------
+gulp.task("browserSync", () => {
+  browserSync.init({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: DIST_FOLDER,
+    },
+    startPath: "main/index.html",
+  });
 });
 
-gulp.task("svg", () => {
-  return gulp
-    .src(SRC_PATH.ASSETS.IMAGES + "/**/*.svg")
-    .pipe(gulp.dest(DEST_PATH.ASSETS.IMAGES))
-    .pipe(browserSync.stream());
+// -------------------- Watch Task --------------------
+gulp.task("watch", () => {
+  gulp.watch(`${SRC_PATH.EJS}/**/*.ejs`, gulp.series("ejs")).on("change", (path) => {
+    console.log(`EJS file changed: ${path}`);
+  });
+  gulp.watch(`${SRC_PATH.ASSETS.SCSS}/**/*.scss`, gulp.series("scss:compile"));
+  gulp.watch(`${SRC_PATH.ASSETS.JS}/**/*.js`, gulp.series("js"));
+  gulp.watch(`${SRC_PATH.ASSETS.SCSS}/**/*.scss`, gulp.series("scss:compile"));
+  gulp.watch(`${SRC_PATH.ASSETS.JS}/**/*.js`, gulp.series("js"));
+  gulp.watch(`${SRC_PATH.ASSETS.IMAGES}/**/*`, gulp.series("images"));
+  gulp.watch(`${SRC_PATH.ASSETS.FONTS}/**/*`, gulp.series("fonts"));
+  gulp.watch(`${SRC_PATH.ASSETS.MOVIES}/*`, gulp.series("movies"));
 });
 
-gulp.task("fonts", () => {
-  return gulp
-    .src(SRC_PATH.ASSETS.FONTS + "/**/*.+(eot|otf|svg|ttf|woff|woff2)")
-    .pipe(gulp.dest(DEST_PATH.ASSETS.FONTS))
-    .pipe(browserSync.stream());
-});
+// -------------------- Deployment --------------------
+gulp.task("gh", () => gulp.src(`${DIST_FOLDER}/**/*`).pipe(ghPages()));
 
-gulp.task("movies", () => {
-  return gulp
-    .src(SRC_PATH.ASSETS.MOVIES + "/*")
-    .pipe(gulp.dest(DEST_PATH.ASSETS.MOVIES))
-    .pipe(browserSync.stream());
-});
+// -------------------- Task Groups --------------------
+const build = gulp.series("clean", gulp.parallel("ejs", "scss:compile", "js", "images", "fonts", "movies"));
+const dev = gulp.series(build, gulp.parallel("browserSync", "watch"));
+const deploy = gulp.series(build, "gh", "cleanDeploy");
 
-gulp.task("watch", function () {
-  gulp.watch(SRC_PATH.EJS + "/**/*.ejs", gulp.series("ejs"));
-  gulp.watch(SRC_PATH.ASSETS.SCSS + "/**/*.scss", gulp.series("scss:compile"));
-  gulp.watch(SRC_PATH.ASSETS.JS + "/**/*.js", gulp.series("js"));
-  gulp.watch(SRC_PATH.ASSETS.IMAGES + "/**/*.+(png|jpg|jpeg|gif|ico)", gulp.series("images"));
-  gulp.watch(SRC_PATH.ASSETS.IMAGES + "/**/*.svg", gulp.series("svg"));
-  gulp.watch(SRC_PATH.ASSETS.FONTS + "/**/*.+(eot|otf|svg|ttf|woff|woff2)", gulp.series("fonts"));
-  gulp.watch(SRC_PATH.ASSETS.MOVIES + "/*", gulp.series("movies"));
-});
-
-gulp.task("browserSync", function () {
-    browserSync.init({
-        notify: false,
-        port: 9000,
-        server: {
-            baseDir: ["dist"],
-            open: true,
-        },
-    });
-});
-
-function clean() {
-    return del(DIST_FOLDER)
-}
-
-function cleanDeploy() {
-  return del(".publish")
-}
-
-gulp.task('gh', function () {
-    return gulp.src(DIST_FOLDER + '/**/*')
-    .pipe(ghPages());
-});
-
-const prepare = gulp.series(clean);
-
-const build = gulp.series(
-    prepare,
-    gulp.parallel("html", "ejs", "scss:compile", "js", "images", "svg", "fonts", "movies")
-);
-
-const buildProd = gulp.series(
-    prepare,
-    gulp.parallel("html", "ejs-prod", "scss:compile", "js", "images", "svg", "fonts", "movies")
-);
-
-function watchFiles() {
-    gulp.watch(SRC_PATH.EJS + "/**/*.ejs", gulp.series("ejs"));
-    gulp.watch(SRC_PATH.ASSETS.SCSS + "/**/*.scss", gulp.series("scss:compile"));
-    gulp.watch(SRC_PATH.ASSETS.JS + "/**/*.js", gulp.series("js"));
-    gulp.watch(SRC_PATH.ASSETS.IMAGES + "/**/*.+(png|jpg|jpeg|gif|ico)", gulp.series("images"));
-    gulp.watch(SRC_PATH.ASSETS.IMAGES + "/**/*.svg", gulp.series("svg"));
-    gulp.watch(SRC_PATH.ASSETS.FONTS + "/**/*.+(eot|otf|svg|ttf|woff|woff2)", gulp.series("fonts"));
-    gulp.watch(SRC_PATH.ASSETS.MOVIES + "/*", gulp.series("movies"));
-}
-
-const defaultTask = gulp.series(clean, build, gulp.parallel("browserSync", watchFiles));
-
-const dev = gulp.series(build, gulp.parallel("browserSync", watchFiles));
-
-const deploy = gulp.series(buildProd, 'gh', cleanDeploy);
-
-module.exports = {
-  clean,
-  prepare,
-  build,
-  watch: watchFiles,
-  default: defaultTask,
-  dev,
-  deploy
-};
+// -------------------- Export Tasks --------------------
+exports.clean = gulp.series("clean");
+exports.build = build;
+exports.dev = dev;
+exports.deploy = deploy;
+exports.default = dev;
